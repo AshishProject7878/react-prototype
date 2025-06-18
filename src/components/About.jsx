6112daf3b08b996d6da0708bd6e623a70b28bc56
Ashishroy1724 <ashishroy78782@gmail.com>
2025-06-18 13:11:57 +0530

    changes 138

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
           ease: "power1.inOut",
           duration: 1,
           scale: 1.2,
-          height: "170dvh",
+          height: "190dvh",
         })
         .to(".knowImg", {
           left: "13%",
@@ -51,7 +51,7 @@ const About = () => {
         .to(
           ".AboutFlexP",
           {
-            y: 150,
+            y: 180,
             opacity: 1,
             zIndex: 1,
             duration: 1,
